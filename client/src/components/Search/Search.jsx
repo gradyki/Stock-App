d482fe4b066f1caca7f1aa13ac7a9af3f37f7314
Grady King <grady.king07@gmail.com>
2021-05-18 17:18:32 -0400

    still working on search render issue

--- client/src/components/Search/Search.jsx
+++ client/src/components/Search/Search.jsx
@@ -1,14 +1,5 @@
 import "./Search.css";
-import { useState } from "react";
-import { useHistory } from "react-router-dom";
 
 export default function Search(prop) {
-  const [search, setSearch] = useState("");
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    useHistory.push(`/test`);
-  };
-
   return <div>search test</div>;
 }
